a3a5976595e7687f2d3e7b577a595ece87abaacc
Trung Pham <trungpham2012@gmail.com>
2019-09-10 13:53:21 -0500

    fix deployment error

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ app.use('/api/cart', cartRouter);
 app.use('/api/items', itemRouter);
 app.use('/api/payment', paymentRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  });
+};
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, err => {
