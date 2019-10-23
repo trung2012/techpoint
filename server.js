b72face6804000307263ce38c4ed178803edf459
Trung Pham <trungpham2012@gmail.com>
2019-10-23 11:46:34 -0500

    rollback

--- server.js
+++ server.js
@@ -29,10 +29,8 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', (req, res, next) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-    res.setHeader('Last-Modified', (new Date()).toUTCString());
-    next();
   });
 };
 
