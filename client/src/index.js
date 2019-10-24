d10fcf7e6e8decd0a4e34468f095fcec18f6c034
Trung Pham <trungpham2012@gmail.com>
2019-10-24 18:52:44 -0500

    safari mobile issue

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,6 @@ import history from './history';
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,8 +18,3 @@ ReactDOM.render(
     </Router>
   </Provider>
   , document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
