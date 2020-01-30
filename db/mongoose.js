503857ac2b9369ab4711ebe6978e371ecc4637bf
Trung Pham <trungpham2012@gmail.com>
2020-01-30 15:59:42 -0600

    fixed some styling

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,7 +4,8 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 mongoose.connect(process.env.db, {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 })
   .then(console.log('MongoDB is connected!'))
   .catch(err => console.log(err))
